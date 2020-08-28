1fe7d6b838e87417740da98bd474953c91284ec5
itoncek4 <51187692+itoncek4@users.noreply.github.com>
2020-08-28 19:11:39 +0200

    přidání steelu

---
+++ src/main/java/cz/itoncek/realmode/item/SteelItem.java
@@ -0,0 +1,46 @@
+
+package cz.itoncek.realmode.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import cz.itoncek.realmode.RealModeModElements;
+
+@RealModeModElements.ModElement.Tag
+public class SteelItem extends RealModeModElements.ModElement {
+	@ObjectHolder("real_mode:steel")
+	public static final Item block = null;
+	public SteelItem(RealModeModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64));
+			setRegistryName("steel");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
